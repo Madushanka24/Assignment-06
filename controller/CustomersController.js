231be40a31290a5ec54c94fc298c41cad9ab6b4f
Madushanka24 <madushanka20021224@gmail.com>
2024-06-07 12:08:50 +0530

    customer JS

--- controller/CustomersController.js
+++ controller/CustomersController.js
@@ -1,4 +1,4 @@
-import CustomerModel from "modle/CustomerModle";
+import CustomerModel from "../model/CustomerModel.js";
 import {customers} from "../db/db.js";
 var recordIndexCustomers;
 
@@ -20,7 +20,7 @@ $('#nav-customers-section').on('click',() => {
     function styleButton(button) {
         button.css({
             background: 'none',
-            color: 'darkslategrey',
+            color: '#B05200',
             padding: '18px 28px',
             border: '30px',
             text: 'none',
@@ -38,13 +38,13 @@ $('#nav-customers-section').on('click',() => {
     function applyHoverEffect(button) {
         button.hover(function () {
             $(this).css({
-                background: 'darkslategrey',
+                background: '#B05200',
                 color: '#FEE5D4'
             });
         }, function () {
             $(this).css({
                 background: 'none',
-                color: 'darkslategrey',
+                color: '#B05200',
                 padding: '18px 28px',
                 border: '30px',
                 text: 'none',
@@ -62,12 +62,44 @@ $('#nav-customers-section').on('click',() => {
     the css style in the orders page btn.This is because all the css is applied to one file (SPA)*/
     $(customers).hover(function (){
         $(this).css({
-            background: 'darkslategrey',
+            background: '#B05200',
             color: '#FEE5D4'
         });
     });
 });
 
+var ValidCustomerID = $('#customers-content-card-left>#txtCustomerID');
+var ValidCustomerName = $('#customers-content-card-left>#txtName');
+var ValidCustomerAddress = $('#customers-content-card-left>#txtAddress');
+var ValidCustomerPhoneNumber = $('#customers-content-card-left>#txtPhoneNumber');
+var isValidCustomerName = new RegExp("\\b[A-Z][a-z]*( [A-Z][a-z]*)*\\b");
+var isValidCustomerAddress = new RegExp("^[A-Za-z0-9'\\/\\.,\\s]{5,}$");
+var isValidPhoneNumber = new RegExp("^(?:0|94|\\+94|0094)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|91)(0|2|3|4|5|7|9)|7(0|1|2|4|5|6|7|8)\\d)\\d{6}$");
+
+
+$(ValidCustomerID).on("input", function () {
+    $(ValidCustomerID).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(ValidCustomerName).on("input", function () {
+    $(ValidCustomerName).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(ValidCustomerAddress).on("input", function () {
+    $(ValidCustomerAddress).css({
+        border: "2px solid #B05200"
+    });
+});
+
+$(ValidCustomerPhoneNumber).on("input", function () {
+    $(ValidCustomerPhoneNumber).css({
+        border: "2px solid #B05200"
+    });
+});
 
 /**Add, Update, Delete, Clear All**/
 
@@ -76,6 +108,65 @@ function clearAll() {
     $('#txtName').val("");
     $('#txtAddress').val("");
     $('#txtPhoneNumber').val("");
+}
+
+function emptyPlaceHolder() {
+    $(ValidCustomerID).attr("placeholder", "");
+    $(ValidCustomerName).attr("placeholder", "");
+    $(ValidCustomerAddress).attr("placeholder", "");
+    $(ValidCustomerPhoneNumber).attr("placeholder", "");
+}
+
+function defaultBorderColor() {
+    $(ValidCustomerID).css({
+        border: "2px solid #B05200"
+    });
+    $(ValidCustomerName).css({
+        border: "2px solid #B05200"
+    });
+    $(ValidCustomerAddress).css({
+        border: "2px solid #B05200"
+    });
+    $(ValidCustomerPhoneNumber).css({
+        border: "2px solid #B05200"
+    });
+}
+
+function validCustomer() {
+    var customerID = $('#txtCustomerID').val();
+    var customerName = $('#txtName').val();
+    var customerAddress = $('#txtAddress').val();
+    var phoneNumber = $('#txtPhoneNumber').val();
+
+    if (customerID === "" || !isValidCustomerName.test(customerName) || !isValidCustomerAddress.test(customerAddress) || !isValidPhoneNumber.test(phoneNumber)) {
+
+        $(ValidCustomerID).css({
+            border: "3px solid red"
+        });
+        $(ValidCustomerName).css({
+            border: "3px solid red"
+        });
+        $(ValidCustomerAddress).css({
+            border: "3px solid red"
+        });
+        $(ValidCustomerPhoneNumber).css({
+            border: "3px solid red"
+        });
+
+        $(ValidCustomerID).attr("placeholder", "ID Empty");
+        $(ValidCustomerName).attr("placeholder", "Wrong Input Try Again");
+        $(ValidCustomerAddress).attr("placeholder", "Wrong Input Try Again");
+        $(ValidCustomerPhoneNumber).attr("placeholder", "Wrong Input Try Again");
+
+        $(ValidCustomerID).addClass('red');
+        $(ValidCustomerName).addClass('red');
+        $(ValidCustomerAddress).addClass('red');
+        $(ValidCustomerPhoneNumber).addClass('red');
+
+    }  else {
+        defaultBorderColor();
+        emptyPlaceHolder();
+    }
 }
 
 function totalCustomers() {
@@ -122,16 +213,34 @@ $('#addCustomers').on('click', () => {
     var customerAddress = $('#txtAddress').val();
     var phoneNumber = $('#txtPhoneNumber').val();
 
+    if (customerID === "" || !isValidCustomerName.test(customerName) || !isValidCustomerAddress.test(customerAddress) || !isValidPhoneNumber.test(phoneNumber)) {
+        validCustomer();
+        return;
+    }
     let customerModel = new CustomerModel(customerID,customerName,customerAddress,phoneNumber);
-
     customers.push(customerModel);
+    defaultBorderColor();
+    emptyPlaceHolder();
     loadCustomerTable();
     clearAll();
     totalCustomers();
 });
 
 $('#btnDelete-customer').on('click',() => {
+
+    var customerID = $('#txtCustomerID').val();
+    var customerName = $('#txtName').val();
+    var customerAddress = $('#txtAddress').val();
+    var phoneNumber = $('#txtPhoneNumber').val();
+
+    if (customerID === "" || !isValidCustomerName.test(customerName) || !isValidCustomerAddress.test(customerAddress) || !isValidPhoneNumber.test(phoneNumber)) {
+        validCustomer();
+        return;
+    }
+
     customers.splice(recordIndexCustomers,1);
+    defaultBorderColor();
+    emptyPlaceHolder();
     loadCustomerTable();
     clearAll();
     totalCustomers();
@@ -144,48 +253,38 @@ $('#btnUpdate-customer').on('click',() => {
     var customerAddress = $('#txtAddress').val();
     var phoneNumber = $('#txtPhoneNumber').val();
 
+    if (customerID === "" || !isValidCustomerName.test(customerName) || !isValidCustomerAddress.test(customerAddress) || !isValidPhoneNumber.test(phoneNumber)) {
+        validCustomer();
+        return;
+    }
+
     var cOb = customers[recordIndexCustomers];
     cOb.id = customerID;
     cOb.name = customerName;
     cOb.address = customerAddress;
     cOb.phoneNumber = phoneNumber;
-
+    defaultBorderColor();
+    emptyPlaceHolder();
     loadCustomerTable();
     clearAll();
     totalCustomers();
 });
 
 function searchCustomers(query) {
-    const searchTerm = query.toLowerCase(); // Convert the search query to lowercase for case-insensitive search
-    const searchResults = customers.filter(customer => {
-        // Check if the customer ID or phone number contains the search term
-        return customer.id.toLowerCase().includes(searchTerm) || customer.phoneNumber.toLowerCase().includes(searchTerm);
-    });
+    const searchTerm = query.toLowerCase();
 
-    // Render search results
-    renderSearchResults(searchResults);
+    for (let i = 0; i < customers.length; i++) {
+        if (searchTerm === customers[i].id.toLowerCase() || searchTerm === customers[i].phoneNumber.toLowerCase()) {
+            $('#txtCustomerID').val(customers[i].id);
+            $('#txtName').val(customers[i].name);
+            $('#txtAddress').val(customers[i].address);
+            $('#txtPhoneNumber').val(customers[i].phoneNumber);
+            break;
+        }
+    }
 }
 
-function renderSearchResults(results) {
-
-    // Render each search result in the table
-    results.forEach(customer => {
-        const customerRecord = `<tr>
-            <td class="c-id">${customer.id}</td>
-            <td class="c-name">${customer.name}</td>
-            <td class="c-address">${customer.address}</td>
-            <td class="c-phoneNumber">${customer.phoneNumber}</td>
-        </tr>`;
-        $('#customers-table-tb').append(customerRecord);
-
-        $('#txtCustomerID').val(customer.id);
-        $('#txtName').val(customer.name);
-        $('#txtAddress').val(customer.address);
-        $('#txtPhoneNumber').val(customer.phoneNumber);
-    });
-}
-
-$('#txtSearch-customers').on('input', function() {
-    const searchQuery = $(this).val();
+$('#search-customer').on('click', function() {
+    const searchQuery = $('#txtSearch-customers').val();
     searchCustomers(searchQuery);
 });
